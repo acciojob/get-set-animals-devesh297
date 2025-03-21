a6ae20b2f9385e1dc36d2b5c1465c58f71fc0f1a
Devesh Goswami <53081430+devesh297@users.noreply.github.com>
2025-03-22 01:54:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-//complete this code
 class Animal {
-	constructor(species){
-	this.species=species;
-	}
-makeSound{
-	console.log(`The ${this.species} makes a sound`);
-}
+  constructor(species) {
+    this.species = species;
+  }
+
+  makeSound() {
+    console.log(`The ${this.species} makes a sound`);
+  }
 }
 
 class Dog extends Animal {
-	bark(){
-		consloe.log("woof");
-	}
+  bark() {
+    console.log("woof");
+  }
 }
 
 class Cat extends Animal {
-	bark(){
-     consloe.log("purr");
-	
-	}
+  purr() {
+    console.log("purr");
+  }
 }
 
 // Do not change the code below this line
